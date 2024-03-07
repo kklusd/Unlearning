462cad25d404e6948317f07805041d14ad021222
Greatknee <1325452253@qq.com>
2024-03-07 16:47:47 +0800

    updata37_4simplification

--- SimCLR/run.py
+++ SimCLR/run.py
@@ -26,7 +26,7 @@ parser.add_argument('-a', '--arch', metavar='ARCH', default='resnet18',
                          ' (default: resnet50)')
 parser.add_argument('-j', '--workers', default=12, type=int, metavar='N',
                     help='number of data loading workers (default: 32)')
-parser.add_argument('--epochs', default=2, type=int, metavar='N',
+parser.add_argument('--epochs', default=100, type=int, metavar='N',
                     help='number of total epochs to run')
 parser.add_argument('-b', '--batch-size', default=256, type=int,
                     metavar='N',
